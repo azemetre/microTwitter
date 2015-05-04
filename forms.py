35af448adf4232e5ebc26202839112ac106f8d34
Aaron Zemetres <azemetre@gmail.com>
2015-05-04 13:58:35 -0400

    Created PostForm class

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Regexp, ValidationError, Email, Length, EqualTo
 
 from models import User
@@ -50,3 +50,7 @@ class RegisterForm(Form):
 class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+
+class PostForm(Form):
+    content = TextAreaField("What's up?", validators=[DataRequired()])
